be28a1e6d83692e8a56ed3145c4300c69687ba8a
Dimas <dim_er89@outlook.com>
2021-04-20 17:59:13 -0500

    cambios

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,33 +1,18 @@
-document.querySelector('.menu-btn').addEventListener('click', () =>{
-    document.querySelector('.menu').classList.toggle('show');
-    
-});
-
 const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
    document.body.classList.toggle('dark');
    btnSwitch.classList.toggle('active'); 
 });
 
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+windows.addEventListener('load',function(){
+  new Glider(document.querySelector('cards_list'))
 });
-  
+new Glider(document.querySelector('.cards'), {
+  slidesToShow: 1,
+  dots: '#dots',
+  draggable: true,
+  arrows: {
+    prev: '.cards-prev',
+    next: '.cards-next'
+  }
+});
