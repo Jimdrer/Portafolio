f0db1a72e44782a0aa2654f8e739212f25ed8cc8
Jose Dimas Alejandro Espinosa Razo <81352856+Jimdrer@users.noreply.github.com>
2022-03-09 14:45:43 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  btnSwitch.classList.toggle('active'); 
+});
+
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".cont");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("activ");
+    });
+    e.target.classList.add("activ");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("activ");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("activ");
+  }
+});
