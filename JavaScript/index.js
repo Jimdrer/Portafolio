d1e2099dc7b6db47e7a6bbf1e3d50be0f94385df
Jose Dimas Alejandro Espinosa Razo <81352856+Jimdrer@users.noreply.github.com>
2022-06-21 12:18:37 -0500

    Add files via upload

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,17 @@
+// addEventListener('DOMContentLoaded', () => {
+//     const menu_btn = document.querySelector('.menu-btn')
+//     if (menu_btn){
+//         menu_btn.addEventListener('click', () => {
+//             const menu_nav = document.querySelector('.nav-menu')
+//             menu_nav.classList.toggle('show')
+//         })
+//     }
+// })
+
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
 $(function(){
     $('.c').easyPieChart({
         size:120,
