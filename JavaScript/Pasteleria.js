4d79b5c83f12ab7cf38ef547f7a373bb1e0fd74c
Jimdrer <Bherzecker@gmail.com>
2025-04-18 14:31:00 -0600

    1.18

--- JavaScript/Pasteleria.js
+++ JavaScript/Pasteleria.js
@@ -13,3 +13,18 @@ tabBtn.forEach((tabbtn,i) =>{
     tabbtn.classList.add('active');
   })
 })
+
+import SimpleParallax from "simple-parallax-js/vanilla";
+const image = document.getElementsByClassName('thumbnail');
+new SimpleParallax(image);
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image);
+var image = document.querySelectorAll('.thumbnail');
+new SimpleParallax(image, {
+    delay: 0,
+    orientation: 'down',
+    scale: 1.3,
+    overflow: true,
+    customContainer: '.container',
+    customWrapper: '.wrapper'
+});
