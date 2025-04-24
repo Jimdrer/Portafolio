f1e1bd19a78b340c3784694a6476e56b6ca5b74c
Jimdrer <Bherzecker@gmail.com>
2025-04-23 23:11:20 -0600

    18.3

--- JavaScript/Pasteleria.js
+++ JavaScript/Pasteleria.js
@@ -14,17 +14,18 @@ tabBtn.forEach((tabbtn,i) =>{
   })
 })
 
-import SimpleParallax from "simple-parallax-js/vanilla";
-const image = document.getElementsByClassName('thumbnail');
-new SimpleParallax(image);
-var image = document.getElementsByClassName('thumbnail');
-new simpleParallax(image);
-var image = document.querySelectorAll('.thumbnail');
-new SimpleParallax(image, {
-    delay: 0,
-    orientation: 'down',
-    scale: 1.3,
-    overflow: true,
-    customContainer: '.container',
-    customWrapper: '.wrapper'
-});
+let flour = document.getElementById("flour");
+let straw = document.getElementById("straw");
+let logo = document.getElementById("logo");
+let cup = document.getElementById("cup");
+let text = document.getElementById("text");
+
+window.addEventListener('scroll', function(){
+  var value = window.scrollY;
+  
+  flour.style.top = value * 0.5 + 'px';
+  straw.style.top = -value *.10 + 'px';
+  logo.style.left = -value *.5 + 'px';
+  cup.style.top = value * .15 + 'px';
+  text.style.top = value * 2 + 'px';
+})
