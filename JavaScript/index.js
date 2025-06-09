2751010066dc266354714c2e2ed289fefe796600
Jimdrer <Bherzecker@gmail.com>
2025-06-09 15:02:39 -0600

    1.18.1

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,10 +1,8 @@
 function descargarArchivo(){
     const seleccionado = document.querySelector('input[name="archivo"]:checked').value;
     
-    // Crear un enlace temporal para la descarga
     const link = document.createElement('a');
     link.href = "https://drive.proton.me/urls/H4QBBPVQ70#ZcuFEGiMiMv9";
-    link.download = "https://drive.proton.me/urls/H4QBBPVQ70#ZcuFEGiMiMv9";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -15,6 +13,6 @@ function descargarArchivo(){
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    // Opcional: Mostrar mensaje
+
     alert(`Iniciando descarga de ${seleccionado}`);
 };
