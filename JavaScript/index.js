c8f75726421321365365086f7d2a35375c455737
Jimdrer <Bherzecker@gmail.com>
2025-06-09 15:29:07 -0600

    1.18.2

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,18 +1,11 @@
 function descargarArchivo(){
-    const seleccionado = document.querySelector('input[name="archivo"]:checked').value;
+    const archivo = document.querySelector('input[name="archivo"]:checked').value;
     
+    // Crear un enlace temporal para la descarga
     const link = document.createElement('a');
-    link.href = "https://drive.proton.me/urls/H4QBBPVQ70#ZcuFEGiMiMv9";
+    link.href = archivo;
+    link.download = archivo;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-
-    const link1 = document.createElement('b');
-    link.href = "https://drive.proton.me/urls/PZ3481TVC8#LZSFWOZgjVn1";
-    link.download = "https://drive.proton.me/urls/PZ3481TVC8#LZSFWOZgjVn1";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-
-    alert(`Iniciando descarga de ${seleccionado}`);
 };
